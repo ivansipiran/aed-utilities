f047c147cea1e9094fff8693a379a8880032becd
ivan <isipiran@pucp.edu.pe>
2021-04-28 09:10:35 -0500

    Support Nodoe

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'aed_utilities',         # How you named your package folder (MyLib)
   packages = ['aed_utilities'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '0.5.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Herramientas basicas de dibujo de estructuras de datos',   # Give a short description about your library
   author = 'Profesores del Curso',                   # Type in your name
   author_email = 'isipiran@dcc.uchile.cl',      # Type in your E-Mail
   url = 'https://github.com/ivansipiran/aed_utilities',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ivansipiran/aed-utilities/archive/refs/tags/v0.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ivansipiran/aed-utilities/archive/refs/tags/v0.5.1.tar.gz',    # I explain this later on
   keywords = ['estructuras de datos', 'algoritmos'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
